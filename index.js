a28e8383bea440cb28d0bb458066aa950281c8dc
md-imtiaj <89583416+md-imtiaj@users.noreply.github.com>
2022-05-16 21:07:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
+
+http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
   res.end(msg);
-});
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
 });
